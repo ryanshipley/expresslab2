dbf109c8472c807025811e0f3b5f3cb54afe6885
Ryan Shipley <ryanshipley@Ryans-MacBook-Pro.local>
2022-08-29 16:30:13 -0400

    Greeting express application created

--- server.js
+++ server.js
@@ -5,4 +5,10 @@ app.get("/", (req, res)=>{
     res.send("Hello World!");
 });
 
+app.get("/greeting/:name?", (req, res)=>{
+    if(req.params.name){
+        res.send("What's up, " + req.params.name + " It's so great to see you!");
+    };
+});
+
 app.listen(3000);
